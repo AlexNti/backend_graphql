8ae0f97302faae1235634dccd6d203a35e9d9a81
AlexNti <a.ntitoras@gmail.com>
2019-05-24 19:29:14 +0300

    end of14 vide

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const Mutations = {};
+const Mutations = {
+   async createItem(parent,args,ctx,info){
+        const item = await ctx.db.mutation.createItem({
+            data:{
+                ...args
+            }
+        },info);
+        return item;
+    }
+    // createDog(parent,args,ctx,info){
+    //     global.dogs= global.dogs||[];
+    //     //create a dog
+    //     const newDog = {name: args.name};
+    //     global.dogs.push(newDog);
+    // }
+};
 
 module.exports = Mutations;
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,3 +1,17 @@
-const Query = {};
+const {forwardTo} =require('prisma-binding');
+
+const Query = {
+    items:forwardTo('db')
+}; //forward to prism without athenctication etc...
+
+
+// const Query = {
+// async items(parent,args,ctx,info){
+//     const items = await ctx.db.query.items();
+//     return items;
+
+// }
+
+// };
 
 module.exports = Query;
