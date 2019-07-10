fd683ceb373ea3ba26743ebd30ffc8d7f5fb7931
AlexNti <a.ntitoras@gmail.com>
2019-07-10 19:42:09 +0300

    end of video 24

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+const jwt = require("jsonwebtoken");
 const Mutations = {
    async createItem(parent,args,ctx,info){
         const item = await ctx.db.mutation.createItem({
@@ -24,13 +26,29 @@ const Mutations = {
 
         return ctx.db.mutation.deleteItem({where},info);
 
+    },
+    async signup(parent,args,ctx,info){
+        args.email = args.email.toLowerCase();
+        const password = await bcrypt.hash(args.password,10);
+        const user = await ctx.db.mutation.createUser({
+            data:{
+                ...args,
+                password,
+                permissions: {set:['USER']}
+
+            }
+        },info);
+
+        const token = jwt.sign({userId:user.id},process.env.APP_SECRET);
+        ctx.response.cookie('token',token,{
+            httpOnly : true,
+            maxAge: 1000*60*60*24*365
+        });
+
+        return user;
+         
     }
-    // createDog(parent,args,ctx,info){
-    //     global.dogs= global.dogs||[];
-    //     //create a dog
-    //     const newDog = {name: args.name};
-    //     global.dogs.push(newDog);
-    // }
+
 };
 
 module.exports = Mutations;
