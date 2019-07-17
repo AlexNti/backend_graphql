957083fe955046f695f21ca4654a198a13088e43
AlexNti <a.ntitoras@gmail.com>
2019-07-17 17:03:30 +0300

    end of 43

--- src/index.js
+++ src/index.js
@@ -19,14 +19,14 @@ const cookieParser = require("cookie-parser");
   });
 
   server.express.use(async (req, res, next) => {
-    if(!req.userId) return next();
+    // if they aren't logged in, skip this
+    if (!req.userId) return next();
     const user = await db.query.user(
-      {where:{id:req.userId}},
+      { where: { id: req.userId } },
       '{ id, permissions, email, name }'
-      );
-      req.user = user;
-
-      next();
+    );
+    req.user = user;
+    next();
   });
 
  server.start(
